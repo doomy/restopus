0db60a64c613cc6375219f992eb2cbf161e0b75f
Vladimír Bártek <vladimir.bartek@gmail.com>
2024-07-06 23:34:48 +0200

    Forbidden exception is translated to 403 json response

--- src/Presenter/AbstractRestPresenter.php
+++ src/Presenter/AbstractRestPresenter.php
@@ -10,8 +10,10 @@ use http\Exception\RuntimeException;
 use Nette\Application\IPresenter;
 use Nette\Application\Request;
 use Nette\Application\Response;
+use Nette\Application\Responses\JsonResponse;
 use Nette\DI\Attributes\Inject;
 use Nette\Http\IRequest as HttpRequest;
+use Nette\Http\IResponse;
 use ReflectionMethod;
 
 abstract class AbstractRestPresenter implements IPresenter
@@ -27,32 +29,41 @@ abstract class AbstractRestPresenter implements IPresenter
     #[Inject]
     public HttpRequest $httpRequest;
 
+    #[Inject]
+    public IResponse $httpResponse;
+
     public function run(Request $request): Response
     {
         $rawBody = $this->httpRequest->getRawBody();
+        try {
+            if ($rawBody === null) {
+                throw new ForbiddenException('Request body is missing');
+            }
 
-        if ($rawBody === null) {
-            throw new ForbiddenException('Request body is missing');
+            $bodyDecoded = json_decode($rawBody, true);
+            if (! is_array($bodyDecoded)) {
+                throw new ForbiddenException('Invalid request body');
+            }
+
+            $action = $request->getParameter(self::PARAM_ACTION);
+            if (! is_string($action)) {
+                throw new ForbiddenException();
+            }
+            $methodReflection = $this->getMethodReflection($action);
+
+            $this->requestValidator->validateRequest(
+                actionMethodReflection: $methodReflection,
+                request: $request,
+                requestBody: $bodyDecoded
+            );
+
+            $response = $methodReflection->invoke($this, $bodyDecoded);
+        } catch (ForbiddenException $exception) {
+            $this->httpResponse->setCode(403);
+            return new JsonResponse([
+                'message' => $exception->getMessage(),
+            ]);
         }
-
-        $bodyDecoded = json_decode($rawBody, true);
-        if (! is_array($bodyDecoded)) {
-            throw new ForbiddenException('Invalid request body');
-        }
-
-        $action = $request->getParameter(self::PARAM_ACTION);
-        if (! is_string($action)) {
-            throw new ForbiddenException();
-        }
-        $methodReflection = $this->getMethodReflection($action);
-
-        $this->requestValidator->validateRequest(
-            actionMethodReflection: $methodReflection,
-            request: $request,
-            requestBody: $bodyDecoded
-        );
-        $response = $methodReflection->invoke($this, $bodyDecoded);
-
         if (! $response instanceof Response) {
             throw new RuntimeException('Invalid response type');
         }
